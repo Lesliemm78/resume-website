cb1197ae63882c89d09f4a8108863ac0e375b784
Leslie Moody <lesliemm1978@gmail.com>
2022-04-17 18:28:54 -0400

    newer js script

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function openItem(evt, tabName) {
-  var i, tabcontent, tablinks;
+// function openItem(evt, tabName) {
+//   var i, tabcontent, tablinks;
 
-  tabcontent = document.getElementsByClassName("tabcontent");
+//   tabcontent = document.getElementsByClassName("tabcontent");
 
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
 
-  tablinks = document.getElementsByClassName("tablinks");
+//   tablinks = document.getElementsByClassName("tablinks");
 
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
 
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+//   document.getElementById(tabName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+
+
+const openTab = (clickEvent, tabName) => {
+  const [...tabContent] = document.querySelectorAll('.tabcontent')
+  const [...tabLinks] = document.querySelectorAll('.tablinks')
+  const activeTab = document.querySelector(`#${ tabName }`)
+
+  tabContent.forEach((tab) => tab.style.display = 'none')
+  tabLinks.forEach((tab) => tab.className = tab.className.replace(' active', ''))
+
+  activeTab.style.display = 'block'
+  clickEvent.currentTarget.className += ' active'
 }
