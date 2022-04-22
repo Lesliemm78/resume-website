77402e7ca84163365e3500f887103a0ceac2afcf
Leslie Moody <lesliemm1978@gmail.com>
2022-04-21 22:40:18 -0400

    6-add-tab-interface-for-the-main-content (#9)
    * spelling errors
    
    * I've got Functioning tabs!!!
    
    * newer js script
    
    * tweaks
    
    * lots of new files
    
    * no real change.
    
    * description added

---
+++ script.js
@@ -0,0 +1,35 @@
+// function openItem(evt, tabName) {
+//   var i, tabcontent, tablinks;
+
+import { version } from "typescript"
+
+//   tabcontent = document.getElementsByClassName("tabcontent");
+
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   tablinks = document.getElementsByClassName("tablinks");
+
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   document.getElementById(tabName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+
+
+function openItem(clickEvent, tabName) {
+  const [...tabContent] = document.querySelectorAll('.tabcontent')
+  const [...tabLinks] = document.querySelectorAll('.tablinks')
+  const activeTab = document.querySelector(`#${tabName}`)
+
+  tabContent.forEach((tab) => tab.style.display = 'none')
+  tabLinks.forEach((tab) => tab.className = tab.className.replace(' active', ''))
+
+  activeTab.style.display = 'block'
+  clickEvent.currentTarget.className += ' active'
+}
+//The function is to hide/show the different sections of the webpage on click. 
